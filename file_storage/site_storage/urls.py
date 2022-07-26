6c051ee7f7f9c7c5c90b8d171eb0f339f46a257d
Mukhtar <mukhtar@radix-it.ru>
2022-07-26 17:26:18 +0300

    добавление отображение добавленных фалов

--- file_storage/site_storage/urls.py
+++ file_storage/site_storage/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
+from file_storage import settings
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from django.conf import settings
+
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -10,7 +12,11 @@ urlpatterns = [
     path('book_list/', views.BookView.as_view()),
     path('book_list/<int:pk>/', views.BookDetail.as_view()),
     path('login/', views.user_login.as_view(), name='login'),
-    path('register/', views.register, name = 'register'),
+    path('register/', views.register, name='register'),
     path('', views.user_login.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
